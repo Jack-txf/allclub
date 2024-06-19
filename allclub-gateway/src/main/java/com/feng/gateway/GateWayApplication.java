4891746325ca6ec7343bade4adc1d8018e9a2a65
Jack_txf <762600503@qq.com>
2024-06-19 19:55:03 +0800

    All-club集成微服务，nacos

---
+++ allclub-gateway/src/main/java/com/feng/gateway/GateWayApplication.java
@@ -0,0 +1,16 @@
+package com.feng.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 网关微服务
+ * @author Williams_Tian
+ * @CreateDate 2024/6/19
+ */
+@SpringBootApplication
+public class GateWayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayApplication.class, args);
+    }
+}
