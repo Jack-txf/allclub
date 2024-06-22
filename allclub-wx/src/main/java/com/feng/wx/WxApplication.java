205b44ab3bba423c792cd15f17d0a421ee985d8c
jack_txf <762600503@qq.com>
2024-06-22 23:22:27 +0800

    wx 模块更新

---
+++ allclub-wx/src/main/java/com/feng/wx/WxApplication.java
@@ -0,0 +1,14 @@
+package com.feng.wx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+微信对接服务的启动类--8830
+ */
+@SpringBootApplication
+public class WxApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WxApplication.class, args);
+    }
+}
---
+++ allclub-wx/src/main/java/com/feng/wx/controller/WxController.java
@@ -0,0 +1,72 @@
+package com.feng.wx.controller;
+
+import com.feng.wx.handler.WxChatMsgFactory;
+import com.feng.wx.handler.WxChatMsgHandler;
+import com.feng.wx.utils.MessageUtil;
+import com.feng.wx.utils.SHA1;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.Map;
+import java.util.Objects;
+
+@RestController
+@RequestMapping("/wx/")
+@Slf4j
+public class WxController {
+    @Resource
+    private WxChatMsgFactory wxChatMsgFactory;
+    private static final String token = "txfnbnbnbnb";
+    /*
+     * 回调消息校验 wx配置！！！！！！
+     */
+    @GetMapping("callback")
+    public String callback(@RequestParam("signature") String signature,
+                           @RequestParam("timestamp") String timestamp,
+                           @RequestParam("nonce") String nonce,
+                           @RequestParam("echostr") String echostr) {
+        log.info("get验签请求参数：signature:{}，timestamp:{}，nonce:{}，echostr:{}",
+                signature, timestamp, nonce, echostr);
+        String shaStr = SHA1.getSHA1(token, timestamp, nonce, "");
+        if (signature.equals(shaStr)) {
+            return echostr;
+        }
+        return "unknown";
+    }
+
+
+    /*
+     事件处理
+     */
+    @PostMapping(value = "callback", produces = "application/xml;charset=UTF-8")
+    public String callback(
+            @RequestBody String requestBody,
+            @RequestParam("signature") String signature,
+            @RequestParam("timestamp") String timestamp,
+            @RequestParam("nonce") String nonce,
+            @RequestParam(value = "msg_signature", required = false) String msgSignature) {
+        log.info("接收到微信消息：requestBody：{}", requestBody);
+        Map<String, String> messageMap = MessageUtil.parseXml(requestBody); // 请求体是xml格式的
+        String msgType = messageMap.get("MsgType"); // 消息有类型
+        String event = messageMap.get("Event") == null ? "" : messageMap.get("Event");
+        log.info("msgType:{},event:{}", msgType, event);
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(msgType);
+        if (!StringUtils.isEmpty(event)) {
+            sb.append(".");
+            sb.append(event);
+        }
+        String msgTypeKey = sb.toString();
+        // 策略+工厂模式处理消息类型
+        WxChatMsgHandler wxChatMsgHandler = wxChatMsgFactory.getHandlerByMsgType(msgTypeKey);
+        if (Objects.isNull(wxChatMsgHandler)) {
+            return "unknown";
+        }
+        String replyContent = wxChatMsgHandler.dealMsg(messageMap);
+        log.info("replyContent:{}", replyContent);
+        return replyContent;
+    }
+}
