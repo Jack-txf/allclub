03b4257d5e5fb44172e4e104251126b731d13aab
jack_txf <762600503@qq.com>
2024-06-26 22:49:00 +0800

    20240626-all-club-subject 多线程优化

--- allclub-subject/subject-domain/src/main/java/com/feng/subject/domain/service/impl/SbCategoryDomainServiceImpl.java
+++ allclub-subject/subject-domain/src/main/java/com/feng/subject/domain/service/impl/SbCategoryDomainServiceImpl.java
@@ -8,6 +8,8 @@ import com.feng.subject.domain.entity.SubjectCategoryBO;
 import com.feng.subject.domain.entity.SubjectLabelBO;
 import com.feng.subject.domain.service.SbCategoryDomainService;
 import com.feng.subject.infra.basic.entity.SubjectCategory;
+import com.feng.subject.infra.basic.entity.SubjectLabel;
+import com.feng.subject.infra.basic.entity.SubjectMapping;
 import com.feng.subject.infra.basic.service.SubjectCategoryService;
 import com.feng.subject.infra.basic.service.SubjectLabelService;
 import com.feng.subject.infra.basic.service.SubjectMappingService;
@@ -17,9 +19,11 @@ import org.springframework.util.CollectionUtils;
 
 import javax.annotation.Resource;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ThreadPoolExecutor;
 import java.util.stream.Collectors;
 
 @Service("sbCategoryDomainService")
@@ -31,6 +35,8 @@ public class SbCategoryDomainServiceImpl implements SbCategoryDomainService {
     private SubjectMappingService subjectMappingService;
     @Resource
     private SubjectLabelService subjectLabelService;
+    @Resource
+    private ThreadPoolExecutor labelThreadPool;
 
     @Override
     public void add(SubjectCategoryBO subjectCategoryBO) {
@@ -49,9 +55,8 @@ public class SbCategoryDomainServiceImpl implements SbCategoryDomainService {
         subjectCategory.setCategoryType(CategoryTypeEnum.PRIMARY.getCode()); // 查询大类
 
         List<SubjectCategory> categoryList = subjectCategoryService.queryPrimaryCategory(subjectCategory);
-
         // entity转为BO
-        List<SubjectCategoryBO> boList = SubjectCategoryConverter.INSTANCE.convertBoToCategory(categoryList);
+        List<SubjectCategoryBO> boList = SubjectCategoryConverter.INSTANCE.convertCategoryToBO(categoryList);
         if (log.isInfoEnabled()) {
             log.info("SubjectCategoryController.queryPrimaryCategory.boList:{}",
                     JSON.toJSONString(boList));
@@ -71,7 +76,7 @@ public class SbCategoryDomainServiceImpl implements SbCategoryDomainService {
         // 查询出entityList
         List<SubjectCategory> categoryList = subjectCategoryService.queryCategory(subjectCategory);
         // entityList转为BOList
-        List<SubjectCategoryBO> boList = SubjectCategoryConverter.INSTANCE.convertBoToCategory(categoryList);
+        List<SubjectCategoryBO> boList = SubjectCategoryConverter.INSTANCE.convertCategoryToBO(categoryList);
         if (log.isInfoEnabled()) {
             log.info("SubjectCategoryController.queryPrimaryCategory.boList:{}",
                     JSON.toJSONString(boList));
@@ -92,7 +97,7 @@ public class SbCategoryDomainServiceImpl implements SbCategoryDomainService {
     @Override
     public Boolean delete(SubjectCategoryBO subjectCategoryBO) {
         // BO转为entity
-        // SubjectCategory subjectCategory = SubjectCategoryConverter.INSTANCE.convertBoToCategory(subjectCategoryBO);
+        // SubjectCategoryConverter.INSTANCE.convertBoToCategory(subjectCategoryBO);
         SubjectCategory category = new SubjectCategory();
         category.setId(subjectCategoryBO.getId());
         category.setIsDeleted(IsDeletedFlagEnum.DELETED.getCode());
@@ -100,8 +105,72 @@ public class SbCategoryDomainServiceImpl implements SbCategoryDomainService {
         return update > 0;
     }
 
+    /*
+    点击一个大类之后，直接查出该大类下的二级分类及其对应的标签，故传过来的categoryId在数据库中，他的parentID必是0，传过来的必定是顶级分类！
+    1.顺序查询，遍历二级分类的数组，查询每个二级分类对应的标签
+    2.多线程异步查询，并行查询每个二级分类对应的标签
+     */
     @Override
     public List<SubjectCategoryBO> queryCategoryAndLabel(SubjectCategoryBO subjectCategoryBO) {
-        return null;
+        Long id = subjectCategoryBO.getId(); // categoryId
+        SubjectCategory subjectCategory = new SubjectCategory();
+        subjectCategory.setParentId(id);
+        subjectCategory.setIsDeleted(IsDeletedFlagEnum.UN_DELETED.getCode());
+        // 查询出parentID为categoryId的所有二级分类
+        List<SubjectCategory> subjectCategoryList = subjectCategoryService.queryCategory(subjectCategory);
+
+        List<SubjectCategoryBO> categoryBOList = SubjectCategoryConverter.INSTANCE.convertCategoryToBO(subjectCategoryList);
+        Map<Long, List<SubjectLabelBO>> map = new HashMap<>(); // completableFutures执行完得到的结果
+        // 遍历subjectCategoryList，查询每个category下面的标签（多线程）
+        List<CompletableFuture<Map<Long, List<SubjectLabelBO>>>> completableFutures = categoryBOList.stream().map(category ->
+                CompletableFuture.supplyAsync(() -> getLabelBOList(category), labelThreadPool) // 第一个参数是执行的任务，第二个是线程池
+        ).collect(Collectors.toList());
+        completableFutures.forEach(future -> {
+            try {
+                Map<Long, List<SubjectLabelBO>> resultMap = future.get(); //等待获取结果
+                if (!CollectionUtils.isEmpty(resultMap)) { // 不空
+                    map.putAll(resultMap);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        // 为所有的二级分类设置标签的集合
+        categoryBOList.forEach(categoryBO -> {
+            if (!CollectionUtils.isEmpty(map.get(categoryBO.getId()))) { // 该categoryId下面的标签不为空
+                categoryBO.setLabelBOList(map.get(categoryBO.getId()));
+            }
+        });
+        return categoryBOList;
+    }
+    /*
+    一个categoryID 对应 多个标签（只有一个entry）
+     */
+    private Map<Long, List<SubjectLabelBO>> getLabelBOList(SubjectCategoryBO category) {
+        if (log.isInfoEnabled()) {
+            log.info("getLabelBOList:{}", JSON.toJSONString(category));
+        }
+        Map<Long, List<SubjectLabelBO>> labelMap = new HashMap<>();
+        SubjectMapping subjectMapping = new SubjectMapping();
+        // 在subject_mapping表中查询category是categoryID的
+        subjectMapping.setCategoryId(category.getId());
+        List<SubjectMapping> mappingList = subjectMappingService.queryLabelId(subjectMapping);
+        if (CollectionUtils.isEmpty(mappingList)) {
+            return null;
+        }
+        // 该categoryID下的所有labelId
+        List<Long> labelIdList = mappingList.stream().map(SubjectMapping::getLabelId).collect(Collectors.toList());
+        List<SubjectLabel> labelList = subjectLabelService.batchQueryById(labelIdList);
+        List<SubjectLabelBO> labelBOList = new LinkedList<>();
+        labelList.forEach(label -> {
+            SubjectLabelBO subjectLabelBO = new SubjectLabelBO();
+            subjectLabelBO.setId(label.getId());
+            subjectLabelBO.setLabelName(label.getLabelName());
+            subjectLabelBO.setCategoryId(label.getCategoryId());
+            subjectLabelBO.setSortNum(label.getSortNum());
+            labelBOList.add(subjectLabelBO);
+        });
+        labelMap.put(category.getId(), labelBOList);
+        return labelMap;
     }
 }
