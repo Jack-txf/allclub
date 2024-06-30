229e49208cf64cd71cd71fe0e04ab244bb5bfa7f
jack_txf <762600503@qq.com>
2024-06-30 21:22:00 +0800

    es开始

---
+++ allclub-subject/subject-infra/src/main/java/com/feng/subject/infra/basic/espojo/SubjectInfoEs.java
@@ -0,0 +1,31 @@
+package com.feng.subject.infra.basic.espojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(indexName = "subject_index")
+public class SubjectInfoEs{
+    @Field(type = FieldType.Long)
+    @Id
+    private Long id;
+
+    @Field(type = FieldType.Text, analyzer = "ik_smart")
+    private String subjectName;
+
+    @Field(type = FieldType.Text, analyzer = "ik_smart")
+    private String subjectAnswer;
+
+    @Field(type = FieldType.Keyword)
+    private String createUser;
+
+    @Field(type = FieldType.Date, index = false)
+    private Long createTime;
+}
