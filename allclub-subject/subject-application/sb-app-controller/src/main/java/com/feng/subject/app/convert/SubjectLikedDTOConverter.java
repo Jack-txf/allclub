065c2a73d5d18bf506839c9421e0aa2cd57d3003
jack_txf <762600503@qq.com>
2024-07-04 14:00:43 +0800

    点赞功能的开发

---
+++ allclub-subject/subject-application/sb-app-controller/src/main/java/com/feng/subject/app/convert/SubjectLikedDTOConverter.java
@@ -0,0 +1,21 @@
+package com.feng.subject.app.convert;
+
+import com.feng.subject.app.dto.SubjectLikedDTO;
+import com.feng.subject.domain.entity.SubjectLikedBO;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+/**
+ * 题目点赞表 dto转换器
+ *
+ * @author txf
+ * @since 2024-01-07 23:08:45
+ */
+@Mapper
+public interface SubjectLikedDTOConverter {
+
+    SubjectLikedDTOConverter INSTANCE = Mappers.getMapper(SubjectLikedDTOConverter.class);
+
+    SubjectLikedBO convertDTOToBO(SubjectLikedDTO subjectLikedDTO);
+
+}
---
+++ allclub-subject/subject-domain/src/main/java/com/feng/subject/domain/convert/SubjectLikedBOConverter.java
@@ -0,0 +1,25 @@
+package com.feng.subject.domain.convert;
+
+import com.feng.subject.domain.entity.SubjectLikedBO;
+import com.feng.subject.infra.basic.entity.SubjectLiked;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
+
+/**
+ * 题目点赞表 bo转换器
+ *
+ * @author txf
+ * @since 2024-01-07 23:08:45
+ */
+@Mapper
+public interface SubjectLikedBOConverter {
+
+    SubjectLikedBOConverter INSTANCE = Mappers.getMapper(SubjectLikedBOConverter.class);
+
+    SubjectLiked convertBOToEntity(SubjectLikedBO subjectLikedBO);
+
+    List<SubjectLikedBO> convertListInfoToBO(List<SubjectLiked> subjectLikedList);
+
+}
