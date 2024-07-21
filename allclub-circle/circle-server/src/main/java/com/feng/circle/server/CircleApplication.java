7a432e71b6ce474e9d34700aa43bd999a2e5f17e
jack_txf <762600503@qq.com>
2024-07-21 17:13:49 +0800

    Circle主启动类

--- allclub-circle/circle-server/src/main/java/com/feng/circle/server/CircleApplication.java
+++ allclub-circle/circle-server/src/main/java/com/feng/circle/server/CircleApplication.java
@@ -1,4 +1,17 @@
 package com.feng.circle.server;
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@MapperScan("com.feng.**.dao")
+@ComponentScan("com.feng")
+@EnableFeignClients(basePackages = "com.feng")
 public class CircleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CircleApplication.class, args);
+    }
 }
---
+++ allclub-circle/circle-server/src/main/java/com/feng/circle/server/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.feng.circle.server.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/circle/test")
+public class TestController {
+
+    @GetMapping("/hello")
+    public String testString() {
+        return "Hello";
+    }
+}
