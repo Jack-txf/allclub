62052b180b6d014b5d019a5211e0832fb74cc26d
jack_txf <762600503@qq.com>
2024-07-21 16:59:01 +0800

    包修改

--- allclub-circle/circle-server/src/main/java/com/feng/circle/server/config/interceptor/LoginInterceptor.java
+++ allclub-circle/circle-server/src/main/java/com/feng/circle/server/config/interceptor/LoginInterceptor.java
@@ -1,6 +1,6 @@
 package com.feng.circle.server.config.interceptor;
 
-import com.feng.practice.server.config.context.UserContextHolder;
+import com.feng.circle.server.config.context.UserContextHolder;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.lang.Nullable;
 import org.springframework.web.servlet.HandlerInterceptor;
