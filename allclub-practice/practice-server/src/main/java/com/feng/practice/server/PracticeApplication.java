df08f04b02c0e000e7b0143b755700d88df9fa33
jack_txf <762600503@qq.com>
2024-07-21 17:13:28 +0800

    启动类打开feign扫描

--- allclub-practice/practice-server/src/main/java/com/feng/practice/server/PracticeApplication.java
+++ allclub-practice/practice-server/src/main/java/com/feng/practice/server/PracticeApplication.java
@@ -3,11 +3,13 @@ package com.feng.practice.server;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("com.feng.practice")
 @MapperScan("com.feng.**.dao")
+@EnableFeignClients(basePackages = "com.feng")
 public class PracticeApplication {
     public static void main(String[] args) {
         SpringApplication.run(PracticeApplication.class, args);
